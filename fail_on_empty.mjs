37a162f6b61f0406f6c8806da150b31f27a3756a
Elliana May <me@mause.me>
2022-01-13 05:59:47 +0000

    build: Log found commands

--- fail_on_empty.mjs
+++ fail_on_empty.mjs
@@ -1,6 +1,7 @@
 import { assert } from "console";
 import { readFileSync } from "fs";
-const manifest = JSON.parse(readFileSync("oclif.manifest.json"));
-const success = Object.keys(manifest.commands).length > 0;
+const commands = JSON.parse(readFileSync("oclif.manifest.json")).commands;
+console.log({ collectedCommands: Object.keys(commands) });
+const success = Object.keys(commands).length > 0;
 
 process.exit(success ? 0 : 1);
